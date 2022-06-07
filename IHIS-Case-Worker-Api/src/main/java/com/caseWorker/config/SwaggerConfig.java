39657eb34cdb080541b62b3cb13b4f5133c3d811
karnan131k <sooriyakumarkarnan@gmail.com>
2022-06-07 19:38:34 +0530

    Case worker swagger configuration update

--- IHIS-Case-Worker-Api/src/main/java/com/caseWorker/config/SwaggerConfig.java
+++ IHIS-Case-Worker-Api/src/main/java/com/caseWorker/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 		
 		return new Docket(DocumentationType.SWAGGER_2)
 				   .select()
-				   .apis(RequestHandlerSelectors.basePackage("com.admin.api.controller"))
+				   .apis(RequestHandlerSelectors.basePackage("com.caseWorker.controller"))
 				   .paths(PathSelectors.any())
 				   .build();
 	}
